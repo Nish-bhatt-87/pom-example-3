e797430e0a49e22d30405c32126f2f0170bcc22d
Nish-bhatt-87 <nischal@devbhatt.net>
2024-01-01 01:38:49 +0800

    test

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.pappu.pass.Operations;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Operations.add(203,3));
+        System.out.println(Operations.subtract(1,3));
+    }
+}
